7c1b578ccbee3d9f2c6fc121bd8c8eecf9fd1ebb
Elsynt11 <elsynoh11@gmail.com>
2025-04-23 02:55:17 -0600

    Add responsive navigation, optimize SVG image size, add new js file and images

---
+++ assets/js/styles.js
@@ -0,0 +1,7 @@
+function changeIcon() {
+    const openIcon = document.getElementById('open-icon');
+    const closeIcon = document.getElementById('close-icon');
+
+    openIcon.classList.toggle('d-none');
+    closeIcon.classList.toggle('d-none');
+}
